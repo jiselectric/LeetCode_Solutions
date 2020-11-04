9789aebbaf4b21b1a5e5fdd58c1c647b2293f888
jiselectric9320 <jiselectric9320@gmail.com>
2020-11-04 22:52:06 +0900

    power

---
+++ power.py
@@ -0,0 +1,15 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        if n == 0:
+            return 1.0
+        if n == 1:
+            return x * 1.0
+        if n < 0:
+            return self.myPow(1/x, -n)
+        
+        result = self.myPow(x * x, n // 2)
+        
+        if n % 2 == 1:
+            result = result * x
+        
+        return result
